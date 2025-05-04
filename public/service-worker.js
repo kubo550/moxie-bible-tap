c2d9f9a44f5eb5507f542b8cae81e2337fd1f527
jKurdziel <qwercy142@gmail.com>
2025-05-04 14:52:39 +0200

    remove not static files from cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,14 +2,7 @@ self.addEventListener('install', (event) => {
   console.log('[SW] Installed');
   event.waitUntil(
     caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/src/assets/favicon/logo192.png',
-        '/src/assets/favicon/logo512.png',
-        '/src/assets/favicon/apple-touch-icon.png'
-      ]);
+      return cache.addAll(['/', '/index.html', '/manifest.json']);
     })
   );
 });
